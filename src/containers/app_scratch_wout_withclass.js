728fad56e40542fa05b4d846209bb03ae1fe8a28
Remus1992 <Remington.Henderson@gmail.com>
2018-10-26 16:24:14 -0700

    Sending HOC WithClass element

--- src/containers/app_scratch_wout_withclass.js
+++ src/containers/app_scratch_wout_withclass.js
@@ -2,7 +2,6 @@ import React, {PureComponent} from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit'
-import WithClass from '../hoc/WithClass';
 
 class App extends PureComponent {
     constructor(props) {
@@ -93,7 +92,7 @@ class App extends PureComponent {
         }
 
         return (
-            <WithClass classes={classes.App}>
+            <div className={classes.App}>
                 <button onClick={() => {
                     this.setState({showPersons: true})
                 }}>Show Persons
@@ -105,7 +104,7 @@ class App extends PureComponent {
                     persons={this.state.persons}
                     clicked={this.togglePersonsHandler}/>
                 {persons}
-            </WithClass>
+            </div>
         );
     }
 }
