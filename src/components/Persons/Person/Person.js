2a4a30be3bcfd27d89cfeb07196f0e21c8924417
Remus1992 <Remington.Henderson@gmail.com>
2018-10-27 18:33:40 -0700

    Sending setState toggleClick counter

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -19,7 +19,7 @@ class Person extends Component {
     render() {
         console.log('[Person.js] Inside render()');
         return (
-            <WithClass classes={classes.Person}>
+            <WithClass classes ={classes.Person}>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name}/>
--- src/containers/App.js
+++ src/containers/App.js
@@ -15,8 +15,9 @@ class App extends PureComponent {
                 {id: "03", name: "Mudeegen", age: 26},
             ],
             otherState: "some other value",
-            showPersons: false
-        };
+            showPersons: false,
+            toggleClicked: 0
+        }
     }
 
     componentWillMount() {
@@ -78,7 +79,15 @@ class App extends PureComponent {
 
     togglePersonsHandler = () => {
         const doesShow = this.state.showPersons;
-        this.setState({showPersons: !doesShow});
+        this.setState((prevState, props) => {
+            return {
+                showPersons: !doesShow,
+                //  this is better than using this.state and writing over.
+                // We do this by making a function where the prevState can be passed in
+                toggleClicked: prevState.toggleClicked + 1
+            }
+
+        });
     };
 
     render() {
